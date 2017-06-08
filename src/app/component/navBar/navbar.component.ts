964c2e16a2ae139ef20581bef300cf67b631cd1e
Fritz <fjeanty47@gmail.com>
2017-06-08 10:34:30 -0400

    navigation bar

---
+++ src/app/component/navBar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from "routing/post.service";
+//import { Report1 } from "app/model/report1.model";
+import { Observable } from "rxjs/Observable";
+//import { User } from "app/model/User.Model"
+import { routing } from "routing/routing.service";
+
+
+
+@Component({
+    
+    selector: 'nav-bar',
+   // moduleId: module.id,
+    templateUrl:`./navbar.component.html` ,
+  "styles": ["styles.css"],
+})
+export class NavBarComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+ 
+
+
+       
+}
